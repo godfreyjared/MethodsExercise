90f604ca246a3ef6f7d09b6adc992d6d6690ed62
Jared Godfrey <godfreyjared@gmail.com>
2025-07-14 20:10:04 -0600

    Projects without bonus challenges

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,51 @@
 {
     public class Program
     {
+        public static void Madlib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favBand = Console.ReadLine();
+
+            Console.WriteLine($"One day, {userName} was walking through the woods wearing a {favColor} shirt. \n {userName} saw a {favAnimal}. \n The animal also liked listening to {favBand}.");
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+
+            return numOne - numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+
+            return numOne / numTwo;
+        }
+
+        public static int Muiltiply(int numOne, int numTwo)
+        {
+
+            return numOne * numTwo;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Madlib();
+            Console.WriteLine(Add(numOne:2, numTwo:2));
+            Console.WriteLine(Subtract(numOne: 2, numTwo: 2));
+            Console.WriteLine(Divide(numOne: 2, numTwo: 2));
+            Console.WriteLine(Muiltiply(numOne: 2, numTwo: 2));
         }
     }
 }
